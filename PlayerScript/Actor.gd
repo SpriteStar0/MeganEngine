0d46cf4c9aab94662ecd24f295fa8be3705a4030
Sprite Star <59860827+SpriteStar0@users.noreply.github.com>
2022-08-23 15:16:57 -0500

    Initial commit

---
+++ PlayerScript/Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(275.0, 1000.0)
+export var gravity: = 3200.0
+
+var _velocity: = Vector2.ZERO
+
